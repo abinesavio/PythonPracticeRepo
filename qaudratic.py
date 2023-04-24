12ae0d340dfb2f2eb9575cd05febe48ab4f3ac91
abinesavio <abinesavio111@gmail.com>
2023-04-24 17:07:07 +0530

    implemented quadratic equation

---
+++ qaudratic.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the value of X2"))
+b=int(input("Enter the value of X1"))
+c=int(input("Enter the value of X0"))
+y = (b**2)-(4*a*c)
+D =y **0.5
+x1 = (-b +D)/(2*a)
+x2 = (-b -D)/(2*a)
+print("The roots are:",x1,x2)

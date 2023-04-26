a5c4df2f275deaf954c1ef4b4d5d2fc37216f5eb
abinesavio <abinesavio111@gmail.com>
2023-04-26 14:09:42 +0530

    some problems

---
+++ Homework.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number:"))
+list1 = ["BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY", "BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY",
+         "BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY", "BANANA FRIES", "POTATO CHIPS", "APPLE JUICE", "CANDY"]
+list2 = [12, 30, 10, 5, 12, 30, 10, 5, 12, 30, 10, 5, 12, 30, 10, 5]
+print(n)
+for i in range(n):
+    print(list1[i], list2[i])
---
+++ Notes.py
@@ -0,0 +1,23 @@
+# #function as mainly used do some specified instrunction we initiated
+# two types inbuilt  and user builti
+# # libraries are groups of functions
+# clouded libraris are called package
+#installing a package using cmd
+# pip install package name some packages have some dependency so we may have to install additional package
+#we can import these packages with import
+#libraryname.classname.functionname
+#function name are small and class names are capital letters
+#classes also have paranthessis Studnt() is an example of class
+
+
+#first we need to install the library virtualenv
+#after installing virtualenv   pip install virtualenv
+#to create an env you have to type virtualenv ai_env
+#to activate ai_env/Scripts/activate
+#so here is a probelm so we are installing in our base environ ment
+#installing pip install opencv-python
+#to check wether there is any installed modules  pip freeze
+
+
+
+##about pip

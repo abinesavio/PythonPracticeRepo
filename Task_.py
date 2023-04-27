08e6b0e2cd34c0eb403fe5a611e36e4c27c6673b
abinesavio <abinesavio111@gmail.com>
2023-04-27 11:46:54 +0530

    modified dict problem

--- Task_.py
+++ Task_.py
@@ -1,12 +1,19 @@
-n= int(input("Number of items:"))
-d = {}
-for i in range(n):
-    item = input("Enter the name and rate:").split()
-    ip = int(item[-1])
-    itm = " ".join(item[:-1])
-    if d.get(itm):
-        d[itm] += ip
-    else:
-        d[itm] = ip
-for i in d.keys():
-    print(i, d[i])
+# n= int(input("Number of items:"))
+# d = {}
+# for i in range(n):
+#     item = input("Enter the name and rate:").split()
+#     ip = int(item[-1])
+#     itm = " ".join(item[:-1])
+#     if d.get(itm):
+#         d[itm] += ip
+#     else:
+#         d[itm] = ip
+# for i in d.keys():
+#     print(i, d[i])
+# 2 ND WAY
+order = {}
+for _ in range(int(input("Enter the number:"))):
+    item, space, price =input("Enter the item and price:").rpartition(" ")
+    order[item] = order.get(item,0) + int(price)
+for item ,price in order.items():
+    print(item,price)
